e4c10728c5c15c3a14d6328632791044297af132
Altora Mclean <altoramclean@gmail.com>
2021-11-10 18:47:45 -0500

    its working!

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import Form from './Form';
+import User from './Users';
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
+import * as yup from 'yup';
+import schema from './validation/formSchema';
 
 
 
@@ -22,8 +25,6 @@ const initialFormErrors = {
 const initialUsers = []
 
 const initialDisabled = true
-
-
 
 
 function App() {
@@ -47,49 +48,74 @@ function App() {
       })
   }
 
-const postNewUser = newUser => {
-  axios.post ('https://reqres.in/api/users', newUser)
-    .then(res => {
-      setUsers([newUser, ...users]);
-    }) .catch(err => {
-      console.error(err);
-    }).finally(() => {
-      setFormValues(initialFormValues);
-    })
+  const postNewUser = newUser => {
+    axios.post('https://reqres.in/api/users', newUser)
+      .then(response => {
+        setUsers([response.data, ...users])
+      })
+      .catch(error => {
+        console.error(error)
+      })
+      .finally(() => {
+        setFormValues(initialFormValues)
+      })
+  }
+const validate = (name, value) => {
+  yup.reach(schema, name)
+    .validate(value)
+    .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+    .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
 }
+const inputChange = (name, value) => {
+  validate(name, value);
+  setFormValues({
+    ...formValues,
+    [name]: value 
+  })
+}
+const formSubmit = () =>{
+  const newUser = {
+    first_name: formValues.first_name.trim(),
+    last_name: formValues.last_name.trim(),
+    email: formValues.email.trim(),
+    terms: formValues.terms
+  } 
+  postNewUser(newUser)
+}
+useEffect(() => {
+  getUsers()
+}, [])
+
+useEffect(() => {
+  schema.isValid(formValues).then(valid => setDisabled(!valid))
+}, [formValues])
 
 
 
+return (
+  <div className="App">
 
+    <header>
+      <h1>New User App</h1>
+    </header>
+    
+        <Form 
+          values={formValues}
+          change={inputChange}
+          submit={formSubmit}
+          disabled={disabled}
+          errors={formErrors}
 
+          />
 
+{users.map(item => {
+            return(
+                <User key={item.id} details={item}/>      
+            )
+          })}
 
-
-
-
-
-
-
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
+
   );
 }
-
 export default App;
